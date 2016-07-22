2c8e94cb94f7674667d01b2f8091b2aa0f8d3f57
Kamarudin <k4m4r82@gmail.com>
2016-07-22 21:15:22 +0700

    First commit

---
+++ src/KRSoftwareLib/DayHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KRSoftwareLib
+{
+    public class DayHelper
+    {        
+        public string GetIndonesianDay(DateTime date)
+        {
+            var indonesianDay = string.Empty;
+
+            switch (Weekday(date))   
+            {
+                case 1:
+                    indonesianDay = "Senin";
+                    break;
+                case 2:
+                    indonesianDay = "Selasa";
+                    break;
+                case 3:
+                    indonesianDay = "Rabu";
+                    break;
+                case 4:
+                    indonesianDay = "Kamis";
+                    break;
+                case 5:
+                    indonesianDay = "Jum'at";
+                    break;
+                case 6:
+                    indonesianDay = "Sabtu";
+                    break;
+                default:
+                    indonesianDay = "Minggu";
+                    break;
+            }
+
+            return indonesianDay;
+        }
+
+        private int Weekday(DateTime date)
+        {
+            var startOfWeek = DayOfWeek.Sunday;
+            return (date.DayOfWeek - startOfWeek + 7) % 7;
+        }
+    }
+}
---
+++ src/KRSoftwareLibUnitTest/DayHelperTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using KRSoftwareLib;
+using NUnit.Framework;
+
+namespace KRSoftwareLibUnitTest
+{
+    [TestFixture]
+    public class DayHelperTest
+    {
+        [Test]
+        public void GetIndonesianDayTest()
+        {
+            var helper = new DayHelper();
+
+            Assert.AreEqual("Senin", helper.GetIndonesianDay(new DateTime(2016, 7, 25)));
+            Assert.AreEqual("Selasa", helper.GetIndonesianDay(new DateTime(2016, 7, 26)));
+            Assert.AreEqual("Rabu", helper.GetIndonesianDay(new DateTime(2016, 7, 27)));
+            Assert.AreEqual("Kamis", helper.GetIndonesianDay(new DateTime(2016, 7, 28)));
+            Assert.AreEqual("Jum'at", helper.GetIndonesianDay(new DateTime(2016, 7, 29)));
+            Assert.AreEqual("Sabtu", helper.GetIndonesianDay(new DateTime(2016, 7, 30)));
+            Assert.AreEqual("Minggu", helper.GetIndonesianDay(new DateTime(2016, 7, 31)));
+        }
+    }
+}
